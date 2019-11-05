b203b1a86d0e0f09383b21437e54a8272350b40a
envincebal <vincentyan1@gmail.com>
2019-11-04 22:17:51 -0800

    git commit -m "first commit"

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http.createServer((req, res) => {
+    let address = req.url;
+    let q = url.parse(address, true);
+    let filePath = "";
+
+    if (q.pathname.includes("documentation")) {
+      filePath = (__dirname + "/documentation.html");
+    } else {
+      filePath = "index.html";
+    }
+
+
+    fs.appendFile("log.txt", `URL: ${address} Timestamp: ${new Date()}`, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Added to log.")
+      }
+    })
+  }).listen(8080);
