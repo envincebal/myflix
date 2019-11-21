e67b41ac5b911fb6076a386865e686f27d2e7390
envincebal <vincentyan1@gmail.com>
2019-11-21 02:25:33 -0800

    Added heroku and MongoDB Atlas.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cors({
   }
 }));
 
-mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+mongoose.connect("mongodb+srv://envincebal:.357magnum@myflixdb-luj5p.mongodb.net/test?retryWrites=true&w=majority", {
   useNewUrlParser: true
 });
 
