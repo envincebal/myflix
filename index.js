9555da0197f968622e55fdc5d9fb269a58adbe46
envincebal <vincentyan1@gmail.com>
2019-11-22 01:37:42 -0800

    Removed CORS.

--- index.js
+++ index.js
@@ -13,18 +13,9 @@ const {
 const Movies = Models.Movie;
 const Users = Models.User;
 
-var allowedOrigins = ['http://localhost:3000', 'http://testsite.com'];
+// var allowedOrigins = ['http://localhost:3000', 'http://testsite.com'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 mongoose.connect("mongodb+srv://envincebal:.357magnum@myflixdb-luj5p.mongodb.net/test?retryWrites=true&w=majority", {
   useNewUrlParser: true
