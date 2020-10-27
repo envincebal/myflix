0a1a09f5b76a60dad4f36bbc0f3c8b366b87f541
envincebal <vincentyan1@gmail.com>
2020-10-27 15:27:05 -0700

    Added date validator.

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.post("/users",
     .isEmpty(),
   check("Password", "Password must be at least 8 characters long").isLength({min: 8}),
   check("Email", "Email does not appear to be valid").isEmail(),
-  check("BirthDate").isISO8601()
+  check("BirthDate").isDate()
 ], (req, res) => {
 
   // check the validation object for errors
